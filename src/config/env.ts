b22c13850ff4d5c7d8c733e99ec5a234a7c8c35c
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2025-04-09 11:04:36 -0300

    typo fixed

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,12 +6,12 @@ const environment = process.env.ENV || "LOCAL";
 
 const telegramBotToken =
   environment === "PROD"
-    ? process.env.PRD_TELEGRAM_BOT_TOKEN
+    ? process.env.PROD_TELEGRAM_BOT_TOKEN
     : process.env.LOCAL_TELEGRAM_BOT_TOKEN;
 
 const botServiceURL =
   environment === "PROD"
-    ? process.env.PRD_BOT_SERVICE_URL
+    ? process.env.PROD_BOT_SERVICE_URL
     : process.env.LOCAL_BOT_SERVICE_URL;
 
 export const env = {
