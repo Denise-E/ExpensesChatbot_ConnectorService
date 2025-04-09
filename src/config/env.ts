d4960a166e790b4308b16af012cd484e761a46b1
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2025-04-09 10:59:28 -0300

    env variables updated

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// Defines .env variables
+// .env variables
+const environment = process.env.ENV || "LOCAL";
+
+const telegramBotToken =
+  environment === "PROD"
+    ? process.env.PRD_TELEGRAM_BOT_TOKEN
+    : process.env.LOCAL_TELEGRAM_BOT_TOKEN;
+
+const botServiceURL =
+  environment === "PROD"
+    ? process.env.PRD_BOT_SERVICE_URL
+    : process.env.LOCAL_BOT_SERVICE_URL;
+
 export const env = {
-  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN,
-  botServiceURL: process.env.BOT_SERVICE_URL,
+  telegramBotToken,
+  botServiceURL,
 };
